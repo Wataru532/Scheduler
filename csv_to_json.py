ed1f15d062f783e977d2a4b73a66ccffae2a1d82
Wataru <167991659+Wataru532@users.noreply.github.com>
2024-05-23 21:23:46 +0900

    nextweek

--- csv_to_json.py
+++ csv_to_json.py
@@ -12,7 +12,7 @@ def csv_to_json(csv_file_path, json_file_path):
             json_file.write(json_data)
 
 if __name__ == '__main__':
-    csv_file_path = 'data/クラス一覧_may24.csv'    # 入力となるCSVファイルのパス
+    csv_file_path = 'data/クラス一覧_may27.csv'    # 入力となるCSVファイルのパス
     json_file_path = 'data/schedule_data.json' # 出力するJSONファイルのパス
     
     csv_to_json(csv_file_path, json_file_path)
